0f240df06fa04b3ec6554d66949aa1f6aa33e228
carlsrecovery8 <135087150+carlsrecovery8@users.noreply.github.com>
2023-06-08 23:50:35 -0500

    Create var.php

---
+++ var.php
@@ -0,0 +1,44 @@
+<?php 
+ $rhversion = "2.0.0"; 
+ $white = "\e[97m"; 
+ $black = "\e[30m\e[1m"; 
+ $yellow = "\e[93m"; 
+ $orange = "\e[38;5;208m"; 
+ $blue   = "\e[34m"; 
+ $lblue  = "\e[36m"; 
+ $cln    = "\e[0m"; 
+ $green  = "\e[92m"; 
+ $fgreen = "\e[32m"; 
+ $red    = "\e[91m"; 
+ $magenta = "\e[35m"; 
+ $bluebg = "\e[44m"; 
+ $lbluebg = "\e[106m"; 
+ $greenbg = "\e[42m"; 
+ $lgreenbg = "\e[102m"; 
+ $yellowbg = "\e[43m"; 
+ $lyellowbg = "\e[103m"; 
+ $redbg = "\e[101m"; 
+ $grey = "\e[37m"; 
+ $cyan = "\e[36m"; 
+ $bold   = "\e[1m"; 
+ function HTNTK_banner(){ 
+   echo "\e[91;1m 
+            una herramienta de\e[36m graficas de informacion para\e[91m And\e[32m escanear Vulnerabilidades \e[91m 
+                                                               
+                                                               
+                                                           
+                                                            
+               . |  (\.|\/|./)  | .   ___   ____ 
+      ###     ###    @@@@@@@@@    .. |   | /   / 
+      ###     ###   @@@@@@@@@@@   |  |   |/   / 
+      ###########   @@@@@@@@@@@      |       / 
+      ###########\e[93m Ver  2.0.0\e[91m  /) \ 
+      ###     ###   @@@@@@@@@@@      |   |\   \ 
+      ###     ###    @@@@@@@@@       |___| \___\ 
+                                                                 
+          {C} Coded By - M7TEAMHK A.K.A HKGATO        
+                                                                   \/ \e[97m [$] Shout Out - You ;) 
+ \e[32m 
+   \n"; 
+ } 
+ ?>
